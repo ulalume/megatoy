19f58a3d837ad9606ace680b828a3bae7f27b5d4
ulalume <ulalume.dev@gmail.com>
2025-11-10 16:55:59 +0900

    Update SDL3 dependency to release-3.2.26
    Bumps the SDL3 version from release-3.2.4 to release-3.2.26 in deps.cmake to use the latest stable release.

--- cmake/deps.cmake
+++ cmake/deps.cmake
@@ -26,7 +26,7 @@ FetchContent_MakeAvailable(nfd)
 FetchContent_Declare(
   SDL3
   GIT_REPOSITORY https://github.com/libsdl-org/SDL
-  GIT_TAG        release-3.2.4
+  GIT_TAG        release-3.2.26
 )
 FetchContent_MakeAvailable(SDL3)
 
