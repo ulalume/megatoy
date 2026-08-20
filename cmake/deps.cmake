9a7df23f5c4bbc3eb147ca50a5deebe1b59d9fb3
ulalume <ulalume.dev@gmail.com>
2026-08-20 20:37:55 +0900

    Bump ym2612_format to v0.2.3

--- cmake/deps.cmake
+++ cmake/deps.cmake
@@ -135,7 +135,7 @@ FetchContent_MakeAvailable(IconFontCppHeaders)
 FetchContent_Declare(
   ym2612_format
   GIT_REPOSITORY https://github.com/ulalume/ym2612_format
-  GIT_TAG        v0.2.2
+  GIT_TAG        v0.2.3
 )
 set(YM2612_FORMAT_BUILD_CLI OFF CACHE BOOL "" FORCE)
 set(YM2612_FORMAT_BUILD_TESTS OFF CACHE BOOL "" FORCE)
